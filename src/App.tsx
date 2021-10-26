e8da1e725657132fff9e3faa49c9679373a69b6c
Jason Reeder <info@jasonreeder.com>
2021-10-26 12:37:49 -0700

    Add react spring animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import { Box, Grommet } from 'grommet';
+import { useSpring, animated } from 'react-spring';
+
 import React from 'react';
 
 function App() {
+  const styles = useSpring({
+    loop: { reverse: true },
+    from: { x: 0 },
+    to: { x: 100 },
+  });
+
   return (
     <Grommet plain full>
       <Box fill align="center" justify="center">
-        <Box
-          height="xsmall"
-          width="xsmall"
-          align="center"
-          justify="center"
-          background="brand"
-          round="xlarge"
-          animation="pulse"
+        <animated.div
+          style={{
+            width: 80,
+            height: 80,
+            backgroundColor: '#46e891',
+            borderRadius: 16,
+            ...styles,
+          }}
         />
       </Box>
     </Grommet>
