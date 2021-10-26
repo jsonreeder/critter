24aadbe71786b14cfee24e91d2030b88aec21808
Jason Reeder <info@jasonreeder.com>
2021-10-26 12:56:09 -0700

    Move on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useSpring, animated } from 'react-spring';
-
-import React from 'react';
+import React, { useRef } from 'react';
 import { Circle, Layer, Stage } from 'react-konva';
 
 function App() {
-  const styles = useSpring({
-    loop: { reverse: true },
-    from: { x: 0 },
-    to: { x: 100 },
-  });
+  const creature = useRef<any>(null);
 
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Circle x={200} y={200} width={50} height={50} fill="purple" />
+        <Circle
+          x={200}
+          y={200}
+          width={50}
+          height={50}
+          fill="purple"
+          ref={creature}
+          onClick={() => creature.current.to({ x: 100 })}
+        />
       </Layer>
     </Stage>
   );
