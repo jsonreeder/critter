a283d3493a5cf6d05d48898ddc1c0882f88055bd
Jason Reeder <info@jasonreeder.com>
2021-11-01 17:34:24 -0700

    Change image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Konva from 'konva';
 import { Easings } from 'konva/lib/Tween';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, MouseEvent } from 'react';
 import { Image, Layer, Stage } from 'react-konva';
 
 import useImage from 'use-image';
@@ -31,6 +31,22 @@ function App() {
     'https://firebasestorage.googleapis.com/v0/b/critter-8c09a.appspot.com/o/plant.png?alt=media&token=1ed59c0d-b854-4ef6-830c-1a605e82883e';
   const [imagePlant] = useImage(urlPlant);
 
+  const [imageJump0] = useImage(
+    'https://firebasestorage.googleapis.com/v0/b/critter-8c09a.appspot.com/o/jump%2Fjump0.png?alt=media&token=5d429073-f5e9-47c2-9503-89b7ed2ca68a',
+  );
+  const [imageJump1] = useImage(
+    'https://firebasestorage.googleapis.com/v0/b/critter-8c09a.appspot.com/o/jump%2Fjump1.png?alt=media&token=4ac56d32-19c5-46c9-9bdb-f49d8fdcdf60',
+  );
+  const [imageJump2] = useImage(
+    'https://firebasestorage.googleapis.com/v0/b/critter-8c09a.appspot.com/o/jump%2Fjump2.png?alt=media&token=823e1e7d-75ec-4113-a79c-2f6ad08c2605',
+  );
+  const [imageJump3] = useImage(
+    'https://firebasestorage.googleapis.com/v0/b/critter-8c09a.appspot.com/o/jump%2Fjump3.png?alt=media&token=768feef6-e1c8-437c-98a0-0c62fad4845e',
+  );
+  const [imageJump4] = useImage(
+    'https://firebasestorage.googleapis.com/v0/b/critter-8c09a.appspot.com/o/jump%2Fjump4.png?alt=media&token=8d5b313f-4fc2-4440-b870-43e5eb39d588',
+  );
+
   const critterSize = 100;
   const width = window.innerWidth;
   const height = window.innerHeight;
@@ -58,10 +74,11 @@ function App() {
     });
   };
 
-  const jump = () => {
-    if (!critter.current) return;
-    critter.current.to({
-      y: critter.current.y() - 50,
+  const jump = (event: Konva.KonvaEventObject<any>) => {
+    const target = event.target as Konva.Image;
+    target.image(imageJump1);
+    target.to({
+      y: target.y() - 50,
       duration: 0.2,
     });
   };
@@ -83,7 +100,7 @@ function App() {
 
   useEffect(() => {
     // moveRecursively();
-    jump();
+    // jump();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleClick = (event: any) => {
@@ -113,6 +130,7 @@ function App() {
           x={initialX}
           y={initialY}
           ref={critter}
+          onClick={jump}
         />
       </Layer>
     </Stage>
